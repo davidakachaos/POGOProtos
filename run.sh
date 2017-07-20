a101be9ecf8240e88a293c37d330997d40b9e4b3
goedzo <git@nims.nl>
2017-07-21 01:34:34 +0200

    Fixed run.sh to remove proto files

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd /src/pogoprotos/
-python compile.py python --keep_proto_files
+python compile.py python
+tar -zcvf out.tar.gz out
+cp out.tar.gz /tmp/
+
 cd out
 cp -Rf pogoprotos ../../pgoapi/pgoapi/protos
 cd /src
