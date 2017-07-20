31751f061594490157d3bc88c25d8b4d796fabf3
goedzo <git@nims.nl>
2017-07-21 01:17:09 +0200

    Compile pgo api

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ cd /src/pogoprotos/
 python compile.py python --keep_proto_files
 cd out
 cp -Rf pogoprotos ../../pgoapi/pgoapi/protos
+cd /src
+tar -zcvf pgoapi.tar.gz pgoapi
+cp pgoapi.tar.gz /tmp/
