448212339994bf5a347ee67a56a1f3a40efa6e53
goedzo <git@nims.nl>
2017-07-22 00:03:25 +0200

    Updated to use flexible folder structure

--- split_discover_protos.bat
+++ split_discover_protos.bat
@@ -1,3 +1,3 @@
 mode con: cols=200 lines=60
-Powershell.exe -File split_discover_protos.ps1 -SourceFolder "E:\Projecten\GIT Repos\POGOProtos\goedzo\src\POGOProtos" -OutFile "E:\Projecten\GIT Repos\POGOProtos\goedzo\proto_index.txt" 
+Powershell.exe -File split_discover_protos.ps1 -SourceFolder "%cd%\src\POGOProtos" -OutFile "%cd%\proto_index.txt" 
 pause
